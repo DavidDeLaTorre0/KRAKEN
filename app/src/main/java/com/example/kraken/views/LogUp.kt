b680f9b8941dc5d7c562349875d519700c4bc40f
david de la torre campo <datocam98@gmail.com>
2025-02-27 23:27:32 +0100

    Fix girar pantalla

--- app/src/main/java/com/example/kraken/views/LogUp.kt
+++ app/src/main/java/com/example/kraken/views/LogUp.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -44,101 +45,111 @@ fun LogUpScreen(auth: FirebaseAuth, db:FirebaseFirestore, navigateToLogin: () ->
     val emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$".toRegex()
 
 
-    Column(
+    LazyColumn(
     modifier = Modifier
         .fillMaxSize()
         .padding(top = 80.dp),
     verticalArrangement = Arrangement.spacedBy(16.dp),
     horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Logo o título "KRAKEN"
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text("KRAKEN", fontSize = 80.sp, color = Texto)
+
+        item {
+            // Logo o título "KRAKEN"
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("KRAKEN", fontSize = 80.sp, color = Texto)
+            }
         }
-
-        Spacer(modifier = Modifier.height(50.dp))
-
-        // Campo de usuario
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            CustomTextField(
-                text = "Nombre usuario:",
-                value = username,
-                onValueChange = { username = it }
-            )
+        item {
+            Spacer(modifier = Modifier.height(50.dp))
         }
-
-        // Campo de contraseña
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            CustomTextField(
-                text = "Contraseña:",
-                value = password,
-                onValueChange = { password = it },
-                isPassword = true,
-            )
+        item {
+            // Campo de usuario
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                CustomTextField(
+                    text = "Nombre usuario:",
+                    value = username,
+                    onValueChange = { username = it }
+                )
+            }
         }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            CustomTextField(
-                text = "Repita la contraseña:",
-                value = password2,
-                onValueChange = { password2 = it },
-                isPassword = true
-            )
+        item {
+            // Campo de contraseña
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                CustomTextField(
+                    text = "Contraseña:",
+                    value = password,
+                    onValueChange = { password = it },
+                    isPassword = true,
+                )
+            }
         }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            CustomTextField(
-                text = "Correo:",
-                value = email,
-                onValueChange = { email = it }
-            )
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                CustomTextField(
+                    text = "Repita la contraseña:",
+                    value = password2,
+                    onValueChange = { password2 = it },
+                    isPassword = true
+                )
+            }
+        }
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                CustomTextField(
+                    text = "Correo:",
+                    value = email,
+                    onValueChange = { email = it }
+                )
+            }
         }
         // Mostrar mensaje de error si es necesario
         if (errorMessage.isNotEmpty()) {
+            item {
                 Text(
                     text = errorMessage,
                     color = Color.Red,
                     fontSize = 14.sp
                 )
+            }
         }
         // Botones
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Column(
-                modifier = Modifier.padding(8.dp)
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
             ) {
-                Button(
-                    onClick = {
+                Column(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Button(
+                        onClick = {
 
-                        //Validaciones
+                            //Validaciones
 
-                        if (password != password2) {
-                            errorMessage = "Las contraseñas no coinciden."
-                        } else if (email.isBlank() || !email.matches(emailPattern)) {
+                            if (password != password2) {
+                                errorMessage = "Las contraseñas no coinciden."
+                            } else if (email.isBlank() || !email.matches(emailPattern)) {
 
-                            errorMessage = "Correo invalido, reviselo"
-                        }
-                        else {
-                                if(username.isBlank() || password.isBlank()){
+                                errorMessage = "Correo invalido, reviselo"
+                            } else {
+                                if (username.isBlank() || password.isBlank()) {
                                     errorMessage = "No puedes dejar campos vacios"
-                                }else{
+                                } else {
                                     errorMessage = ""
                                     // Intentar crear el usuario
                                     auth.createUserWithEmailAndPassword(email, password)
@@ -154,11 +165,11 @@ fun LogUpScreen(auth: FirebaseAuth, db:FirebaseFirestore, navigateToLogin: () ->
                                                 if (user != null) {
                                                     db.collection("usuarios")
                                                         .document(user.uid)
-                                                        .set(userMap).addOnCompleteListener{
+                                                        .set(userMap).addOnCompleteListener {
                                                             Log.i("CUsuario", "Success")
-                                                        }.addOnFailureListener{
+                                                        }.addOnFailureListener {
                                                             Log.i("CUsuario", "Failure")
-                                                        }        .addOnCompleteListener{
+                                                        }.addOnCompleteListener {
                                                             Log.i("CUsuario", "Complete")
                                                         }
                                                 }
@@ -167,26 +178,28 @@ fun LogUpScreen(auth: FirebaseAuth, db:FirebaseFirestore, navigateToLogin: () ->
                                                 navigateToLogin()
                                             } else {
                                                 Log.i("REGIS", "Registro KO")
-                                                errorMessage = "El correo ya esta asociado a otra cuenta"
+                                                errorMessage =
+                                                    "El correo ya esta asociado a otra cuenta"
                                             }
                                         }
                                 }
 
-                        }
-                    },
-                    colors = ButtonDefaults.buttonColors(containerColor = Boton),
-                    modifier = Modifier.width(270.dp)
-                ) {
-                    Text("Registrarse", fontSize = 14.sp)
-                }
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Boton),
+                        modifier = Modifier.width(270.dp)
+                    ) {
+                        Text("Registrarse", fontSize = 14.sp)
+                    }
 
 
-                Button(
-                    onClick = { navigateToLogin() },
-                    colors = ButtonDefaults.buttonColors(containerColor = Boton),
-                    modifier = Modifier.width(270.dp)
-                ) {
-                    Text("Volver", fontSize = 14.sp)
+                    Button(
+                        onClick = { navigateToLogin() },
+                        colors = ButtonDefaults.buttonColors(containerColor = Boton),
+                        modifier = Modifier.width(270.dp)
+                    ) {
+                        Text("Volver", fontSize = 14.sp)
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/kraken/views/Login.kt
+++ app/src/main/java/com/example/kraken/views/Login.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -29,114 +30,123 @@ import com.example.kraken.ui.theme.Texto
 import com.google.firebase.auth.FirebaseAuth
 
 
-
 @Composable
 fun LoginScreen(auth: FirebaseAuth, navigateToHome: () -> Unit, navigateToLogUp: () -> Unit) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var errorMessage by remember { mutableStateOf("") }
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(top = 80.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Logo o título "KRAKEN"
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text("KRAKEN", fontSize = 80.sp, color = Texto)
+
+        item {
+            // Logo o título "KRAKEN"
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("KRAKEN", fontSize = 80.sp, color = Texto)
+            }
         }
-
-        Spacer(modifier = Modifier.height(50.dp))
-
-        // Campo de usuario
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            CustomTextField(
-                text = "Nombre usuario:",
-                value = email,
-                onValueChange = { email = it }
-            )
+        item {
+            Spacer(modifier = Modifier.height(50.dp))
         }
-
-        // Campo de contraseña
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            CustomTextField(
-                text = "Contraseña:",
-                value = password,
-                onValueChange = { password = it },
-                isPassword = true
-            )
+        item {
+            // Campo de usuario
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                CustomTextField(
+                    text = "Nombre usuario:",
+                    value = email,
+                    onValueChange = { email = it }
+                )
+            }
+        }
+        item {
+            // Campo de contraseña
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                CustomTextField(
+                    text = "Contraseña:",
+                    value = password,
+                    onValueChange = { password = it },
+                    isPassword = true
+                )
+            }
         }
         // Mensaje de error
         if (errorMessage.isNotEmpty()) {
-            Text(
-                text = errorMessage,
-                color = Color.Red,
-                fontSize = 14.sp,
-                modifier = Modifier.padding(top = 4.dp)
-            )
-        }
-        // Botones
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Column(
-                modifier = Modifier.padding(8.dp)
-            ) {
-                Button(
-                    onClick = {
-                        if(email.isBlank() || password.isBlank()){
-                            errorMessage = "Email o contraseña no pueden estar vacíos"
-                        //stringResource(R.string.error_login_failed)
-                        //LocalContext.current.getString(R.string.error_login_failed)
-                        }else{
-                            errorMessage = ""
-                            auth.signInWithEmailAndPassword(email, password)
-                                .addOnCompleteListener { task ->
-
-                                    if (task.isSuccessful) {
-                                        //Navegar
-
-                                        Log.i("LOGIN", "LOGIN OK")
-                                        navigateToHome()
-
-                                    } else {
-                                        //Error
-                                        Log.i("LOGIN", "LOGIN KO")
-                                        errorMessage = "Error al iniciar sesión"
-                                    }
-                                }
-                        }
-                    },
-                    colors = ButtonDefaults.buttonColors(containerColor = Boton),
-                    modifier = Modifier.width(125.dp)
-                ) {
-                    Text("Entrar", fontSize = 14.sp)
-                }
+            item {
+                Text(
+                    text = errorMessage,
+                    color = Color.Red,
+                    fontSize = 14.sp,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
             }
-
-            Column(
-                modifier = Modifier.padding(8.dp)
+        }
+        item {
+            // Botones
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
             ) {
-                Button(
-                    onClick = {
-                        navigateToLogUp()
-                    },
-                    colors = ButtonDefaults.buttonColors(containerColor = Boton),
-                    modifier = Modifier.width(125.dp)
+                Column(
+                    modifier = Modifier.padding(8.dp)
                 ) {
-                    Text("Registrarse", fontSize = 14.sp)
+                    Button(
+                        onClick = {
+                            if (email.isBlank() || password.isBlank()) {
+                                errorMessage = "Email o contraseña no pueden estar vacíos"
+                                //stringResource(R.string.error_login_failed)
+                                //LocalContext.current.getString(R.string.error_login_failed)
+                            } else {
+                                errorMessage = ""
+                                auth.signInWithEmailAndPassword(email, password)
+                                    .addOnCompleteListener { task ->
+
+                                        if (task.isSuccessful) {
+                                            //Navegar
+
+                                            Log.i("LOGIN", "LOGIN OK")
+                                            navigateToHome()
+
+                                        } else {
+                                            //Error
+                                            Log.i("LOGIN", "LOGIN KO")
+                                            errorMessage = "Error al iniciar sesión"
+                                        }
+                                    }
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Boton),
+                        modifier = Modifier.width(125.dp)
+                    ) {
+                        Text("Entrar", fontSize = 14.sp)
+                    }
+                }
+
+                Column(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Button(
+                        onClick = {
+                            navigateToLogUp()
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Boton),
+                        modifier = Modifier.width(125.dp)
+                    ) {
+                        Text("Registrarse", fontSize = 14.sp)
+                    }
                 }
             }
         }
